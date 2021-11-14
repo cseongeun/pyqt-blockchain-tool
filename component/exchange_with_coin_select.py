2f3290e0324d4392ae95e4449549b030e7749814
조성은 <eun@hexlant.com>
2021-11-14 20:05:43 +0900

    .

--- component/exchange_with_coin_select.py
+++ component/exchange_with_coin_select.py
@@ -5,30 +5,40 @@ class ExchangeCoinSelectGroup(QWidget):
     def __init__(self):
         super().__init__()
 
-        self.EXCHANGE_INSTANCE = {
-            "binance": binance.Binance(),
-            "upbit": upbit.Upbit()
+        self.EXCHANGES = {
+            "binance": {
+                "instance": binance.Binance(),
+                "checked": False,
+            },
+            "upbit": {
+                "instance": upbit.Upbit(),
+                "checked": False,
+            },
         }
+
+
+        self.coins = []
+
 
     def generate_exchange_group(self):
         groupbox = QGroupBox('exchange')
         vbox = QVBoxLayout()
 
-        for index, exchange in enumerate(list(self.EXCHANGE_INSTANCE.keys())):
+        for index, exchange in enumerate(list(self.EXCHANGES.keys())):
             cb = QCheckBox(exchange, self)
             cb.move(20, 20 * index)
             cb.toggle()
-            cb.stateChanged.connect(lambda state, checkbox=exchange: self.change_exchange_state(state, checkbox) )
+            cb.stateChanged.connect(lambda state, checkbox=exchange: self.change_exchange_state(state, checkbox))
             vbox.addWidget(cb)
 
         groupbox.setLayout(vbox)
         return groupbox
 
-    def generate_coin_group(self, coins=['BTC', 'ETH']):
+    def generate_coin_group(self):
         groupbox = QGroupBox('coin')
         vbox = QVBoxLayout()
 
-        for index, coin in enumerate(coins):
+        for index, coin in enumerate(self.coins):
             cb = QCheckBox(coin, self)
             cb.move(20, 20 * index)
             cb.toggle()
@@ -41,12 +51,22 @@ class ExchangeCoinSelectGroup(QWidget):
     def change_exchange_state(self, state, origin: str):
         boolean = False if state == 0 else True
         print("Checkbox exchange {} has changed: {}".format(origin, boolean))
+        self.EXCHANGES[origin]['checked'] = boolean
 
-        print(self.EXCHANGE_INSTANCE[origin].getCoins())
-        a = self.generate_coin_group(['BAAA'])
-        a.update()
+        self.coins = []
+        for exchange in self.EXCHANGES:
+            checked = self.EXCHANGES[exchange]['checked']
+            print(checked)
+            if checked:
+                self.coins.append(self.EXCHANGES[exchange]['instance'].getCoins())
+
+        return self.coins
 
 
+
+        # selected_exchange_coin = self.EXCHANGE_INSTANCE[origin].getCoins()
+        # self.coins = self.coins.append(selected_exchange_coin)
+        # print(self.coins)
 
     def change_coin_state(self, state, origin: str):
         boolean = False if state == 0 else True
